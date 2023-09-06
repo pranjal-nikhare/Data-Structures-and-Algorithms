c46799521e24262837b0ee300db7b87184ff5a9a
Pranjal Nikhare <91018218+pranjal-nikhare@users.noreply.github.com>
2023-09-06 13:48:16 +0530

    Create DisjointSet.java

---
+++ Graphs/DisjointSet.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+class DisjointSet {
+    int[] rank;
+    int[] parent;
+
+    public DisjointSet(int n) {
+        rank = new int[n+1];
+        parent = new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            rank[i] = 0;
+            parent[i] = i;
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        int ulp = findUPar(parent[node]);
+        parent[node] = ulp;
+        return parent[node];
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+}
+
+class Main {
+    public static void main (String[] args) {
+        DisjointSet ds = new DisjointSet(7);
+        ds.unionByRank(1, 2);
+        ds.unionByRank(2, 3);
+        ds.unionByRank(4, 5);
+        ds.unionByRank(6, 7);
+        ds.unionByRank(5, 6);
+
+        // if 3 and 7 same or not
+        if (ds.findUPar(3) == ds.findUPar(7)) {
+            System.out.println("Same");
+        } else
+            System.out.println("Not Same");
+
+        ds.unionByRank(3, 7);
+        if (ds.findUPar(3) == ds.findUPar(7)) {
+            System.out.println("Same");
+        } else
+            System.out.println("Not Same");
+    }
+}
